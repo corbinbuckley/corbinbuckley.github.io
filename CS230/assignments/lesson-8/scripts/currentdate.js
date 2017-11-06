cabd39c7b444665dfd4e071c8b6f5c3711b5ab7e
Corbin Buckley <corbinbuckley@gmail.com>
2017-11-06 11:53:17 -0700

    Add function for current date

--- CS230/assignments/lesson-8/scripts/currentdate.js
+++ CS230/assignments/lesson-8/scripts/currentdate.js
@@ -1,8 +1,5 @@
-window.onload = function () {
-    currentDate();
 
     function currentDate() {
         var d = new Date();
         document.getElementById("currentdate").innerHTML = d.toDateString();
     };
-}
