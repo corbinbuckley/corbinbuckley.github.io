c10d4ac1ee222887eb1af6776d421dae0c0ae87f
Corbin Buckley <corbinbuckley@gmail.com>
2017-12-12 19:28:45 -0700

    Add external script and cdn

---
+++ CS230/Final/scripts/sales.js
@@ -0,0 +1,10 @@
+
+  $( function() {
+    $( "#accordion" ).accordion({
+        active: false,
+        collapsible: true, 
+        heightStyle: "content", 
+        icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+    });
+  } );
+  
